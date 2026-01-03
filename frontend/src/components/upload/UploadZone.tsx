56837e3773c78dd0bbae14127bf6d69294bb6d45
llOrtegall <IvanOrtega_97@hotmail.com>
2026-01-02 19:04:25 -0500

    fix: adjust styles in UploadZone for improved readability and responsiveness

--- frontend/src/components/upload/UploadZone.tsx
+++ frontend/src/components/upload/UploadZone.tsx
@@ -67,8 +67,8 @@ export default function UploadZone({
         {...getRootProps()}
         className={`bg-green-3 rounded-full 
           h-64 w-64 lg:h-72 lg:w-72 2xl:h-96 2xl:w-96
-          flex cursor-pointer items-center justify-center border border-dashed border-green-4 transition-colors ${isDragActive ? 'bg-green-900' : ''
-          } ${isUploading ? 'opacity-50 cursor-not-allowed' : ''}`}
+          flex cursor-pointer items-center justify-center border border-dashed border-green-4 transition-colors 
+          ${isDragActive ? 'bg-green-900' : ''} ${isUploading ? 'opacity-50 cursor-not-allowed' : ''}`}
       >
         <section className="flex flex-col items-center space-y-1.75">
           <input {...getInputProps()} type="file" />
@@ -77,11 +77,27 @@ export default function UploadZone({
             <img src={MyIcon} width={55} alt="Upload icon" />
           </figure>
 
-          <h2 className="font-imb-600 text-[16px] text-gray-1">
+          <h2 className="
+            font-imb-600 
+            text-[10px] 
+            lg:text-[15px]
+            2xl:text-[16px] 
+            text-gray-1"
+          >
             <p>Click or Drag to Upload</p>
           </h2>
-          <p className="font-imb-400 text-[10px] text-gray-2">Files automatically deleted after 3 days</p>
-          <p className="font-imb-400 text-[10px] text-gray-2">Max Upload File: 1GB</p>
+          <p className="
+            font-imb-400 
+            text-[8px] 
+            lg:text-[9px]
+            2xl:text-[10px]
+           text-gray-2">Files automatically deleted after 3 days</p>
+          <p className="
+            font-imb-400 
+            text-[8px] 
+            lg:text-[9px]
+            2xl:text-[10px]
+           text-gray-2">Max Upload File: 1GB</p>
 
         </section>
       </article>
