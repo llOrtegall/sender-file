47727f91ddee44c9ca47e9a35aa6bb5ffb3c8104
llOrtegall <IvanOrtega_97@hotmail.com>
2026-01-02 22:39:39 -0500

    refactor: remove image prop and simplify SocialLink component structure

--- frontend/src/components/ui/SocialLink.tsx
+++ frontend/src/components/ui/SocialLink.tsx
@@ -1,28 +1,20 @@
 type SocialLinkProps = {
   href: string;
   ariaLabel: string;
-  image?: {
-    src: string;
-    alt: string;
-  };
   text?: string;
 };
 
-export const SocialLink = ({ href, ariaLabel, image, text }: SocialLinkProps) => {
+export const SocialLink = ({ href, ariaLabel, text }: SocialLinkProps) => {
   return (
     <li className="flex items-center">
       <a
-        className="rounded-full bg-green-4 size-6 xl:size-8 2xl:size-10 flex items-center justify-center cursor-pointer hover:bg-green-500"
+        className=""
         target="_blank"
         rel="noopener noreferrer"
         href={href}
         aria-label={ariaLabel}
       >
-        {image ? (
-          <img src={image.src} alt={image.alt} className="size-3.5 xl:size-5 2xl:size-6" />
-        ) : (
-          <span className="text-black text-[10px] xl:text-[14px] 2xl:text-[18px]">{text}</span>
-        )}
+        <span className="text-gray-1 text-[10px] xl:text-[14px] 2xl:text-[18px] hover:text-green-800 px-1">{text}</span>
       </a>
     </li>
   );
